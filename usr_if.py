6b1f5fc5b0bec940f541fa5d7dba0ca3ab360347
rrios19 <ronaldrios19@icloud.com>
2023-04-16 04:21:44 +0100

    Update

--- usr_if.py
+++ usr_if.py
@@ -18,8 +18,11 @@ import socket
 import logging
 import subprocess
 
+# For development
 host_name = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
+host_ip = subprocess.check_output("hostname -I",shell=True,universal_newlines=True).strip()
+#host_ip = socket.gethostbyname(host_name + ".local")
+#host_ip = socket.gethostbyname(host_name + '.')
 print('----------DEBUG----------')
 print(f'Host: {host_name}')
 print(f'IP: {host_ip}')
@@ -37,32 +40,46 @@ def fetch_conf(mod):
         print(f"Can not open local configuration file.\n{err}\nExiting")
         sys.exit()
 
+# Init basic log configurations
 def configure_log():
     path = conf['logpath']
+    levels = {'DEBUG':10,'INFO':20,'WARNING':30,'ERROR':40,'CRITICAL':50}
     if not os.path.exists(path):
         print(f"Creating new log file '{path}'")
     form = '%(levelname)s %(asctime)s %(message)s'
     date = '%H:%M:%S %d/%m/%y'
-    verbose = logging.DEBUG
+    try:
+        verbose = levels[conf['verbose']]
+    except:
+        print(f"Can not set {conf['verbose']} mode. Check local_conf.json file")
+        print(f"Available: {list(levels.keys())}")
+        sys.exit()
     logging.basicConfig(filename=path,format=form,datefmt=date,level=verbose,filemode='a')
+    logging.info(f"Start session from '{host_name}' at '{host_ip}'")
+    logging.info(f"Basic log configuration in '{conf['verbose']}' mode")
 
 class macro:
+    # Create a new macro object for the current test
     def __init__(self,cmd,params,values):
         self.cmd      = cmd
         self.params   = params
         self.values   = values
         self.macro    = {}
         self.str_json = ''
+        logging.debug(f"New macro object created: '{self.cmd}'")
 
+    # Print the current macro
     def show_macro(self):
         print(f"{self.cmd}, {self.params}, {self.values}")
 
+    # Join the parameters and the values
     def join_macro(self):
         pair = {par:val for (par,val) in zip(self.params,self.values)} 
         self.macro = {self.cmd:pair}
-        logging.debug(f"Macro was joined: {self.macro}")
+        logging.debug(f"Macro joined: {self.macro}")
         return self.macro
 
+    # Create a new JSON file with the macro test
     def set_json(self):
         self.str_json = json.dumps(self.macro,indent=2) 
         FH = open(conf['filename'],'w')
@@ -71,14 +88,20 @@ class macro:
         logging.debug(f"JSON file was written: '{conf['filename']}'")
         return self.str_json   
 
+    # Send JSON to MCI
     def send_json(self):
         target = conf['target']
         filename = conf['filename']
         subprocess.run(['scp',filename,f"{target}:mci_iptc/tmp/{filename}"])
+        logging.info(f"Sent {filename} to {host_ip}")
+
+    # Experimental
+    def receive_handler():
+        print('hello')
 
 # Fetch usr_if configurations
 conf = fetch_conf('usr')
-# Basic configuration for log file
+# Basic configuration for log
 configure_log()
 
 # Save arguments
@@ -91,7 +114,5 @@ new = macro(cmd,params,values)
 new.join_macro()
 new.set_json()
 # Send macro
-#new.send_json()
-
-
+new.send_json()
 
